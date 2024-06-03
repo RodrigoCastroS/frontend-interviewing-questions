1a882b66214c0e949393815b0943ee32b681e9ab
Rodrigo C <rodrigocsastre@gmail.com>
2024-06-03 16:57:47 +0200

    Implement find in Tree challenge

--- questions/domTree.js
+++ questions/domTree.js
@@ -1,3 +1,24 @@
 // We have two identical DOM trees, A and B. For DOM tree A, we have
 // the location of an element. Create a function to find that same element
 // in tree B.
+
+function findSameElementOnDOM(element, treeB) {
+    let treePath = []
+    let current = element
+
+    // find the reverse path up to the root element
+    while (current.parentNode){
+        let elementIndex = [...current.parentNode.children].indexOf(element)
+        treeAPath.push(elementIndex)
+        current = current.parentNode
+    }
+
+    current = treeB
+    while(treePath.length){
+        current = current.children[treePath.pop()]
+    }
+
+    return current;
+
+
+}
