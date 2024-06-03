21fb1251637316f2e7336fb9f0ab0849df250071
Rodrigo C <rodrigocsastre@gmail.com>
2024-06-03 16:57:17 +0200

    feat: implement debounce challenge

--- questions/debounce.js
+++ questions/debounce.js
@@ -1,1 +1,17 @@
 // Implement debounce
+
+function debounce(callback, time){
+    let intervalID;
+
+    return function(){
+        if(intervalID){ 
+            clearTimeout(id)
+        }
+
+         intervalID = setTimeout(()=> {
+            callback()
+        }, time)
+
+    }
+
+}
