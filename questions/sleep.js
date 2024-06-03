1619ff02d8950ff7d9804afdd9038314a18fe55c
Rodrigo C <rodrigocsastre@gmail.com>
2024-06-03 17:01:41 +0200

    Implement a sleep function to return a promise given the time to wait

--- questions/sleep.js
+++ questions/sleep.js
@@ -1,6 +1,17 @@
 // Create a sleep function that takes one parameter (time) and
 // will wait "time" ms
 
+function sleep(time){
+    return new Promise((res, rej)=> {
+        setTimeout(()=>{
+            res()
+        }, time)
+
+        rej(err => console.log(err))
+    })
+}
+
+
 /*
     async function run() {
         await sleep(500);
